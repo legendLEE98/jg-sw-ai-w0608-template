01826c1535237e8855f5e3e48f76ed6696058cbd
legendLEE98 <gravity980530@gmail.com>
2026-04-03 10:30:13 +0900

    C언어

---
+++ Practice/1. print/scanf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    int num1;
+    int num2;
+
+    printf("input 받기 1 \n");
+    scanf("%d", &num1);
+    printf("input 받기 2 \n");
+    scanf("%d", &num2);
+
+    printf("%d\n%d\n", num1, num2);
+    return 0;
+}
---
+++ Practice/2. type/int.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("num : %d\n", num);
+}
