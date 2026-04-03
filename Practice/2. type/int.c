0aa642d1ee2e111c56e8afa7f9c4eaab5738c1d8
legendLEE98 <gravity980530@gmail.com>
2026-04-03 11:47:08 +0900

    int

--- Practice/2. type/int.c
+++ Practice/2. type/int.c
@@ -2,6 +2,8 @@
 
 int main() {
     int num1 = 1;
+    unsigned int num2 = 3000000000;
 
-    printf("num : %d\n", num1);
+    printf("signed INT : %d\n", num1);
+    printf("unsigned INT : %u\n", num2);
 }
