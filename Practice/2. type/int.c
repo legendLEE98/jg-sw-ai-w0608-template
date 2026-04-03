973a30068a8a892edd51bef382639ab6bab49b63
legendLEE98 <gravity980530@gmail.com>
2026-04-03 10:36:15 +0900

    int

--- Practice/2. type/int.c
+++ Practice/2. type/int.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    int num;
+    int num1 = -1;
+    int num2 = 1;
+    unsigned int num3 = -300;
 
-    printf("num : %d\n", num);
+    // u == 음수 표현
+    // d == 일반 표현
+    printf("num : %u\n", num3);
 }
